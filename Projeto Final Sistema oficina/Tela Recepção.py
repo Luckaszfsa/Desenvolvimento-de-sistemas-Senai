470afcc24c74cd552caedf45f63f831a84f3fddd
Lucas Mendes <luckasz.mendes@gmail.com>
2022-05-31 16:55:42 -0300

    update

---
+++ Projeto Final Sistema oficina/Tela Recepção.py
@@ -0,0 +1,69 @@
+from tkinter import *
+
+class Recepcao:
+    def __init__(self):
+        self.janela = Tk()
+        self.titleStyles = ('Arial', 14)
+        self.textStyles = ('Roboto', 12)
+
+        self.section = Frame(self.janela)
+        self.section.pack(padx=100)
+
+        self.titulo = Label(self.section, font=self.titleStyles, text='RECEPÇÃO')
+        self.titulo.pack(padx=0, pady=10)
+
+        self.botaoCadastrar = Button(self.section, command=self.cadastrarCliente,
+                                     font=self.textStyles,
+                                     text='Cadastrar\nCliente',
+                                     width=15)
+        self.botaoCadastrar.pack(pady=4)
+
+        self.botaoOrcamento = Button(self.section, command=self.verOrcamento,
+                                     font=self.textStyles,
+                                     text='Ver\nOrçamento',
+                                     width=15)
+        self.botaoOrcamento.pack(pady=4)
+
+        self.frameVazio1 = Frame(self.section)
+        self.frameVazio1.pack(pady=20)
+
+        self.botaoSair = Button(self.section, command=self.sair,
+                                font=self.textStyles,
+                                text='Sair',
+                                width=15)
+        self.botaoSair.pack(pady=4)
+
+        self.frameVazio2 = Frame(self.section)
+        self.frameVazio2.pack(pady=5)
+        
+        
+        mainloop()
+
+    def cadastrarCliente(self):
+        return
+
+    def verOrcamento(self):
+        return
+    
+    def changeOnHover(botaoCadastrar, colorOnHover, colorOnLeave): 
+       
+        self.botaoCadastrar.bind("<Enter>", func=lambda e: button.config( 
+            background=colorOnHover)) 
+      
+        
+        self.botaoCadastrar.bind("<Leave>", func=lambda e: button.config( 
+            background=colorOnLeave)) 
+      
+      
+        root = Tk() 
+          
+        self.myButton = Button(self.janela, 
+                          text="On Hover - Background Change", 
+                          bg="yellow") 
+        self.botaoCadastrar.pack() 
+        changeOnHover(botaoCadastrar, "red", "yellow") 
+
+    def sair(self):
+        return
+
+minhaTela = Recepcao()
