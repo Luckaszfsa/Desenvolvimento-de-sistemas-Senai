6321e4300931d368e7d3f5ac768db87b6efde1cf
Lucas Mendes <94189592+Luckaszfsa@users.noreply.github.com>
2022-06-01 23:46:01 -0300

    Update Tela Login (Interface).py

--- Projeto Final Sistema oficina/Tela Login (Interface).py
+++ Projeto Final Sistema oficina/Tela Login (Interface).py
@@ -13,28 +13,30 @@ class Tela_de_Login:
           
         #Grid.rowconfigure(self.janela,1,weight=1) 
           
-        photo = PhotoImage(file = r"C:\Users\aluno\Downloads\logo.png" )
-        self.titulo = Label(self.janela,image = photo,compound = LEFT, bg="#363636")
-        self.titulo.pack(padx=5, pady=5)
+        photo = PhotoImage(file = r"C:\Users\lucka\OneDrive\Documentos\SENAI\Logica-de-programa-o-senai\Projeto Final Sistema oficina\logo.png" )
+        login = PhotoImage(file = r"C:\Users\lucka\OneDrive\Documentos\SENAI\Logica-de-programa-o-senai\Projeto Final Sistema oficina\Login.png")
+        senha = PhotoImage(file = r"C:\Users\lucka\OneDrive\Documentos\SENAI\Logica-de-programa-o-senai\Projeto Final Sistema oficina\SENHA.png")
+        self.titulo = Label(self.janela,image = photo, bg="#363636")
+        self.titulo.pack()
 
-        self.frameCima = Frame(self.janela)
+        self.frameCima = Frame(self.janela, bg="#363636")
         self.frameCima.pack()
 
-        self.frameBaixo = Frame(self.janela)
+        self.frameBaixo = Frame(self.janela, bg="#363636")
         self.frameBaixo.pack()
         
 
-        self.loginTxt = Label(self.frameCima, text='LOGIN',width=5)
-        self.loginTxt.pack(side='left',padx=5, pady=5)
+        self.loginTxt = Label(self.frameCima, image = login ,width=50, height=20)
+        self.loginTxt.pack(side='left',padx=5, pady=10)
         self.loginEnt = Entry(self.frameCima, width=30)
         self.loginEnt.pack(side='left',padx=5, pady=5)
 
-        self.senhaTxt = Label(self.frameBaixo, text='SENHA',width=5)
+        self.senhaTxt = Label(self.frameBaixo, image = senha,width=50, height=20)
         self.senhaTxt.pack(side='left',padx=5, pady=5)
         self.senhaEnt = Entry(self.frameBaixo, width=30)
         self.senhaEnt.pack(side='left', padx=5, pady=5)
 
-        self.entrarBotao = Button(command=self.verificaLogin, bg="#00BFFF", text='ENTRAR')
+        self.entrarBotao = Button(command=self.verificaLogin, bg="#00BFFF",relief=RAISED,text='ENTRAR')
         self.entrarBotao.pack(padx=5, pady=5)
 
         self.entrarBotao.bind("<Enter>", self.hoverIn)
@@ -43,7 +45,7 @@ class Tela_de_Login:
         mainloop()
 
     def hoverIn(self, event):
-           event.widget.config(bg="#1E90FF")
+           event.widget.config(bg="#00FF00",fg="white")
 
     def hoverOut(self, event):
             event.widget.config(bg="#00BFFF")
