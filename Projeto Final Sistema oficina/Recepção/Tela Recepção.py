53d7cf183692ae645716a6ed42f18da1d53380c7
Lucas Mendes <94189592+Luckaszfsa@users.noreply.github.com>
2022-06-07 20:47:04 -0300

    Update Tela Recepção.py

--- Projeto Final Sistema oficina/Recepção/Tela Recepção.py
+++ Projeto Final Sistema oficina/Recepção/Tela Recepção.py
@@ -7,47 +7,42 @@ class Recepcao:
         self.janela = Tk()
         self.janela.title('Recepção')
         self.titleStyles = ('Arial', 14)
-        self.textStyles = ('Roboto', 12)
+        self.textStyles = ('Roboto', 13)
         self.janela.configure(bg='#363636')
-
+        self.janela.minsize(width=1200,height=600)
+        
+        self.LoginCanvas = Canvas(self.janela, width = 600, height=370)
+        self.LoginCanvas.pack(expand=1,fill=BOTH)
+        
+        self.Back = PhotoImage(file="back.png")
+        self.LoginCanvas.create_image(0,0,image=self.Back, anchor=NW)
+        
         self.hoverColor1 = '#008000'
         self.hoverColor2 = '#1E90FF'
         self.hoverColor3 = '#FF0000'
 
-        self.section = Frame(self.janela,bg='#363636')
-        self.section.pack(padx=100)
-
-        self.titulo = Label(self.section,bg='#363636',fg='white', font=self.titleStyles, text='RECEPÇÃO')
-        self.titulo.pack(padx=0, pady=10)
-
-        self.botaoCadastrar = Button(self.section, command=self.cadastrarCliente,
-                                     font=self.textStyles,
-                                     text='Cadastrar\nCliente',bg='#4682B4',fg='white',
-                                     width=15)
+        self.botaoCadastrar = Button(self.LoginCanvas,text="Cadastrar\nCliente",font=self.textStyles,width=15,height=4,pady=5,bg="#4682B4",fg="white",relief=RAISED,command=self.cadastrarCliente)
+        self.LoginCanvas.create_window(900,130,window=self.botaoCadastrar)
         self.botaoCadastrar.bind("<Enter>", self.hoverIn1)
         self.botaoCadastrar.bind("<Leave>", self.hoverOut)
-        self.botaoCadastrar.pack(pady=4)
+        
 
-        self.botaoOrcamento = Button(self.section, command=self.verOrcamento,
-                                     font=self.textStyles, bg='#4682B4', fg='white',
-                                     text='Ver\nOrçamento',
-                                     width=15)
+        self.botaoOrcamento = Button(self.LoginCanvas,text="Ver\nOrçamento",font=self.textStyles,width=15,height=4,pady=1,bg="#4682B4",fg="white",relief=RAISED,command=self.verOrcamento)
+        self.LoginCanvas.create_window(900,250,window=self.botaoOrcamento)
         self.botaoOrcamento.bind("<Enter>", self.hoverIn2)
         self.botaoOrcamento.bind("<Leave>", self.hoverOut)
-        self.botaoOrcamento.pack(pady=4)
-
-        self.frameVazio1 = Frame(self.section)
-        self.frameVazio1.pack(pady=20)
-
-        self.botaoSair = Button(self.section,bg='#FF0000',fg='white', command=self.saiu,
-                                font=self.textStyles,text='Sair',width=10)
-        self.botaoSair.bind("<Enter>", self.hoverIn3)
-        self.botaoSair.bind("<Leave>", self.hoverOut2)
-        self.botaoSair.pack(pady=4)
-
-        self.frameVazio2 = Frame(self.section)
-        self.frameVazio2.pack(pady=5)
         
+
+        self.botaoSair = Button(self.LoginCanvas,text="SAIR",font=self.textStyles,width=5,bg="#4682B4",fg="white",pady=5,relief=RAISED,command=self.saiu)
+        self.LoginCanvas.create_window(900,400,window=self.botaoSair)
+        self.botaoSair.bind("<Enter>", self.hoverIn3)
+        self.botaoSair.bind("<Leave>", self.hoverOut)
+       
+
+        self.titulo = Label(self.janela,bg='#363636',fg='white', font=self.titleStyles, text='RECEPÇÃO')
+        self.titulo.pack(padx=0, pady=10)
+
+               
         
         mainloop()
 
