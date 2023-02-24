0e82c95e3318b12930cafdcff3bee8345829246d
Lucas Mendes <luckasz.mendes@gmail.com>
2023-02-24 20:37:38 -0300

    Criando User e Resource

---
+++ JAVA/tcc/src/main/java/com/boxes/tcc/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.boxes.tcc.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.boxes.tcc.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L,"Lucas","lucas@email.com","12345678","12345");
+		return ResponseEntity.ok().body(u);
+	}
+
+}
