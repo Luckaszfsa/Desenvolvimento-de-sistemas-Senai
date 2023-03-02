18034566510fb1e5b15163d58fe3f5a35cda7902
Lucas Mendes <luckasz.mendes@gmail.com>
2023-03-01 23:39:00 -0300

    Criação entidade pedido

--- JAVA/tcc/src/main/java/com/boxes/tcc/entities/User.java
+++ JAVA/tcc/src/main/java/com/boxes/tcc/entities/User.java
@@ -1,12 +1,17 @@
 package com.boxes.tcc.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -24,6 +29,10 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	
+	@JsonIgnore
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {
 	}
@@ -76,6 +85,9 @@ public class User implements Serializable {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	public List<Order> getOrders() {
+		return orders;
+	}
 
 	@Override
 	public int hashCode() {
@@ -93,6 +105,8 @@ public class User implements Serializable {
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
+
+	
 	
 	
 	}
---
+++ JAVA/tcc/src/main/java/com/boxes/tcc/services/OrderService.java
@@ -0,0 +1,29 @@
+package com.boxes.tcc.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.boxes.tcc.entities.Order;
+import com.boxes.tcc.entities.User;
+import com.boxes.tcc.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll() {
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+
+}
