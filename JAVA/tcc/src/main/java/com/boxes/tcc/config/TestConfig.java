8bb78519ff7c746e692d0241844bac504db6db09
Lucas Mendes <luckasz.mendes@gmail.com>
2023-03-08 22:54:23 -0300

    Many to Many + JoinTable

--- JAVA/tcc/src/main/java/com/boxes/tcc/config/TestConfig.java
+++ JAVA/tcc/src/main/java/com/boxes/tcc/config/TestConfig.java
@@ -60,7 +60,16 @@ public class TestConfig implements CommandLineRunner {
 		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, ""); 
 
 		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
-				
+		
+		p1.getCategories().add(cat2);
+		p2.getCategories().add(cat1);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
+		
+		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
+		
 	}
 
 }
--- JAVA/tcc/src/main/java/com/boxes/tcc/entities/Category.java
+++ JAVA/tcc/src/main/java/com/boxes/tcc/entities/Category.java
@@ -5,10 +5,13 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
 
@@ -25,7 +28,8 @@ public class Category implements Serializable {
 	private Long id;
 	private String name;
 	
-	@Transient
+	@JsonIgnore
+	@ManyToMany(mappedBy = "categories")
 	private Set<Product> products = new HashSet<>();
 	
 	public Category() {
